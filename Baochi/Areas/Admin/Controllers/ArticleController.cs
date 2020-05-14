b89b65c848627f75a3360ef93064bf08dbf1ab1e
Vu Trung Kien <kien171200312@st.utc.edu.vn>
2020-05-14 19:11:46 +0700

    update view phan admin

---
+++ Baochi/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baochi.Areas.Admin.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Admin/Article
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Adding()
+        {
+            return View();
+        }
+        public ActionResult Editing()
+        {
+            return View();
+        }
+    }
+}
---
+++ Baochi/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baochi.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Adding()
+        {
+            return View();
+        }
+        public ActionResult Editing()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
